6e3f4a5e6af3a883a11d51834d579e4c43156dcf
nakzyu <59201980+nakzyu@users.noreply.github.com>
2020-05-26 02:13:44 +0900

    Add react-router-dom Add pages Combine Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import Movie from "./pages/Movie";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Header />
+          <Home />
+        </Route>
+        <Route path="/movies" exact>
+          <Header />
+          <Movies />
+        </Route>
+        <Route path="/movie/:id" exact>
+          <Header />
+          <Movie />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-home">Home</div>
+      <div className="header-moives">Movies</div>
+    </div>
+  );
+};
+
+export default Header;
