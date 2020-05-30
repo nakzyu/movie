b6d0542e9f1d66d9ad5a00455a52edf53f0f5284
nakzyu <59201980+nakzyu@users.noreply.github.com>
2020-05-31 01:47:46 +0900

    Fix HomeContent

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./HomeContent.css";
 
 const HomeContent = () => {
-  return <div>home content</div>;
+  return (
+    <div className="home-content">
+      <div>사용된 라이브러리 및 프레임워크</div>
+      <div>"axios": "^0.19.2"</div>
+      <div>"react-router-dom": "^5.2.0"</div>
+      <div>"react-bottom-scroll-listener": "^4.0.0"</div>
+    </div>
+  );
 };
 
 export default HomeContent;
